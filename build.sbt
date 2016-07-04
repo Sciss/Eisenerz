bf94958bcca90f3cde4197f26efa37e175fa154b
Hanns Holger Rutz <contact@sciss.de>
2016-07-04 18:38:19 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,42 @@
+lazy val baseName         = "Eisenerz"
+lazy val baseNameL        = baseName.toLowerCase
+lazy val projectVersion   = "0.1.0-SNAPSHOT"
+
+lazy val commonSettings = Seq(
+  version             := projectVersion,
+  organization        := "de.sciss",
+  description         := "An art project",
+  homepage            := Some(url(s"https://github.com/Sciss/$baseName")),
+  scalaVersion        := "2.11.8",
+  licenses            := Seq(gpl2),
+  scalacOptions      ++= Seq("-deprecation", "-unchecked", "-feature", "-encoding", "utf8", "-Xfuture", "-Xlint"),
+  resolvers           += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/maven-releases/",
+  libraryDependencies ++= Seq(
+    "de.sciss"          %% "fileutil"           % "1.1.1",
+    "de.sciss"          %% "numbers"            % "0.1.1",
+    "de.sciss"          %% "processor"          % "0.4.0",
+    "com.mortennobel"   %  "java-image-scaling" % "0.8.6",  // includes jh filters
+    "de.sciss"          %% "audiowidgets-swing" % "1.10.0",
+    "de.sciss"          %% "desktop"            % "0.7.2",
+    "de.sciss"          %% "guiflitz"           % "0.5.0",
+    "de.sciss"          %% "play-json-sealed"   % "0.4.0",
+    "de.sciss"          %% "kollflitz"          % "0.2.0",
+    // "de.sciss"          %  "submin"             % "0.2.1",
+    "com.github.scopt"  %% "scopt"              % "3.4.0",
+    "de.sciss"          %% "scissdsp"           % "1.2.2",
+    "de.sciss"          %% "scalaaudiofile"     % "1.4.5",
+    "de.sciss"          %% "fscapejobs"         % "1.5.0"
+  )
+)
+
+//lazy val cc_by_nc_nd = "CC BY-NC-ND 4.0" -> url("http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode")
+lazy val gpl2        = "GPL v2+"         -> url("http://www.gnu.org/licenses/gpl-2.0.txt")
+
+lazy val root = Project(id = baseNameL, base = file("."))
+  .settings(commonSettings)
+
+// -------------
+
+//mainClass in assembly := Some("de.sciss.unlike.Main")
+//
+//assemblyJarName in assembly := s"$baseName.jar"
---
+++ src/main/scala/de/sciss/eisenerz/Exposure.scala
@@ -0,0 +1,124 @@
+/*
+ *  Exposure.scala
+ *  (Eisenerz)
+ *
+ *  Copyright (c) 2016 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is published under the GNU General Public License v2+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.eisenerz
+
+import java.awt.Color
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+import de.sciss.file._
+import de.sciss.numbers.Implicits._
+
+import scala.collection.breakOut
+
+object Exposure extends App {
+  val baseDir = userHome / "Documents" / "projects" / "Eisenerz" / "image_work"
+  require(baseDir.isDirectory)
+  val inputs  = baseDir.children(f => f.base.startsWith("lapse_") && f.ext == "jpg")
+  val output  = baseDir / "out_lapse.png"
+
+  var composite: Array[Array[Double]] = _
+  var width : Int = _
+  var height: Int = _
+  require(inputs.nonEmpty)
+  println(s"${inputs.size} images.")
+
+  inputs.sortBy(_.name) /* .take(3) */.zipWithIndex.foreach { case (in, idx) =>
+    println(s"Reading ${in.name}...")
+    val bufIn = ImageIO.read(in)
+    val planes = (0 until 3).map(extractChannel(bufIn, _).flatten)(breakOut): Array[Array[Double]]
+    if (idx == 0) {
+      width     = bufIn.getWidth
+      height    = bufIn.getHeight
+      println(s"width = $width, height = $height")
+      composite = planes
+    } else {
+      require(width  == bufIn.getWidth)
+      require(height == bufIn.getHeight)
+      (planes zip composite).foreach { case (a, b) =>
+        add(a, 0, b, 0, a.length)
+      }
+    }
+    bufIn.flush()
+  }
+  println(s"Writing ${output.name}...")
+  val planeMin  = composite.map(_.min).min
+  val planeMax  = composite.map(_.max).max
+  println(f"Channel values: min = $planeMin%1.2f, max = $planeMax%1.2f")
+  val bufOut    = {
+    val res = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+    val gTmp = res.createGraphics()
+    gTmp.setColor(Color.black)
+    gTmp.fillRect(0, 0, width, height)
+    gTmp.dispose()
+    res
+  }
+  val add       = -planeMin
+  val mul       = 1.0 / (planeMax - planeMin)
+  composite.zipWithIndex.foreach { case (plane, ch) =>
+    fillChannel(plane.grouped(width).toArray, bufOut, chan = ch, add = add, mul = mul)
+  }
+  ImageIO.write(bufOut, "png", output)
+  sys.exit()
+
+  /** Adds input to output. */
+  def add(in: Array[Double], inOff: Int, out: Array[Double], outOff: Int, len: Int): Unit = {
+    var i     = inOff
+    var j     = outOff
+    val stop  = i + len
+    while (i < stop) {
+      out(j) += in(i)
+      i += 1
+      j += 1
+    }
+  }
+
+  def extractChannel(in: BufferedImage, chan: Int): Array[Array[Double]] = {
+    val shift = chan * 8
+    Array.tabulate(in.getHeight) { y =>
+      Array.tabulate(in.getWidth) { x =>
+        val i = (in.getRGB(x, y) >>> shift) & 0xFF
+        i.toDouble / 0xFF
+      }
+    }
+  }
+
+  def fillChannel(in: Array[Array[Double]], out: BufferedImage, chan: Int, add: Double = 0.0, mul: Double = 1.0): Unit = {
+    val shift = chan * 8
+    val mask  = ~(0xFF << shift)
+    for (y <- in.indices) {
+      val v = in(y)
+      for (x <- v.indices) {
+        val d = (v(x) + add) * mul
+        val i = (d.clip(0, 1) * 0xFF + 0.5).toInt << shift
+        val j = out.getRGB(x, y)
+        val k = j & mask | i
+        out.setRGB(x, y, k)
+      }
+    }
+  }
+
+  def mulC(a: Array[Array[Double]], b: Array[Array[Double]], scale: Double = 1.0): Unit = {
+    for (y <- a.indices) {
+      val va = a(y)
+      val vb = b(y)
+      for (x <- va.indices by 2) {
+        val re = va(x) * vb(x)   - va(x+1) * vb(x+1)
+        val im = va(x) * vb(x+1) + va(x+1) * vb(x)
+        va(x)   = re * scale
+        va(x+1) = im * scale
+      }
+    }
+  }
+}
