259dbb5889445eb0b0e80c8c1c1d90f4d58cf83b
Sciss <contact@sciss.de>
2016-07-13 14:13:50 +0200

    two colors, no pwm

--- exposure/src/main/scala/de/sciss/eisenerz/Exposure.scala
+++ exposure/src/main/scala/de/sciss/eisenerz/Exposure.scala
@@ -5,8 +5,10 @@ import com.pi4j.io.gpio.{GpioFactory, PinState, RaspiPin}
 object Exposure {
   def main(args: Array[String]): Unit = {
     val gpio = GpioFactory.getInstance
-    val pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Red", PinState.HIGH)
-    pin.setShutdownOptions(true, PinState.LOW)
+    val pinR = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Red"  , PinState.HIGH)
+    val pinG = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Green", PinState.LOW )
+    pinR.setShutdownOptions(true, PinState.LOW)
+    pinG.setShutdownOptions(true, PinState.LOW)
 
     def infoWait(b: Boolean): Unit = {
       println("--> GPIO state should be: ON")
@@ -14,14 +16,17 @@ object Exposure {
     }
 
     infoWait(true)
-    pin.low()
+    pinR.low()
+    pinG.high()
     infoWait(false)
-    pin.toggle()
+    pinR.toggle()
+    pinG.toggle()
     infoWait(true)
-    pin.toggle()
+    pinR.toggle()
+    pinG.toggle()
     infoWait(false)
     println("--> GPIO state should be: ON for only 1 second")
-    pin.pulse(1000, true)  // true = blocking
+    pinR.pulse(1000, true)  // true = blocking
 
     gpio.shutdown()
   }
