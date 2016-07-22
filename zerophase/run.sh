f7b53ccb6227655a0893c43793afc1dd43ce860a
Sciss <contact@sciss.de>
2016-07-22 01:49:45 +0000

    run.sh

--- zerophase/run.sh
+++ zerophase/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# sudo because we need the same user for jackd and scsynth!
-sudo qjackctl &
+qjackctl &
 sleep 5
+amixer cset numid=1 0  # 0 dB volume
 wpa_cli terminate
 sleep 5
-cd /home/pi/Documents/devel/Eisenerz/accelerate
-sudo java -jar Eisenerz-Accelerate.jar
+cd /home/pi/Documents/devel/Eisenerz/zerophase
+java -jar Eisenerz-ZeroPhase.jar -d 480
 sleep 10
